d0f40e7646e8c66a461b36a2b3c058e40cd476df
Alex Bokii <vanilinka33@gmail.com>
2015-07-13 09:50:48 +0100

    added algorithm to covert decimal number to binary

--- script.js
+++ script.js
@@ -21,47 +21,27 @@ converter.controller('converterController', function($scope) {
         text = convertDecimalToBinary(text);
         $scope.binary = text;
     };
+
 });
 
 function convertDecimalToBinary(arr) {
     for(var i = 0; i < arr.length; i++) {
-        var decimalNumber = arr[i];
-        var binaryNumber = ['0', '0', '0', '0', '0', '0', '0', '0'];
-        if(decimalNumber >= 128) {
-            binaryNumber[0] = 1;
-            decimalNumber = decimalNumber - 128;
-        }
-        if(decimalNumber >= 64) {
-            binaryNumber[1] = 1;
-            decimalNumber = decimalNumber - 64;
-        }
-        if(decimalNumber >= 32) {
-            binaryNumber[2] = 1;
-            decimalNumber = decimalNumber - 32;
-        }
-        if(decimalNumber >= 16) {
-            binaryNumber[3] = 1;
-            decimalNumber = decimalNumber - 16;
-        }
-        if(decimalNumber >= 8) {
-            binaryNumber[4] = 1;
-            decimalNumber = decimalNumber - 8;
-        }
-        if(decimalNumber >= 4) {
-            binaryNumber[5] = 1;
-            decimalNumber = decimalNumber - 4;
-        }
-        if(decimalNumber >= 2) {
-            binaryNumber[6] = 1;
-            decimalNumber = decimalNumber - 2;
-        }
-        if(decimalNumber >= 1) {
-            binaryNumber[7] = 1;
-        }
-        binaryNumber = binaryNumber.join('');
-        arr[i] = binaryNumber;
-    }
+        var decNum = arr[i];
+        var binNum = [];
 
+        while(decNum > 0) {
+            if(decNum % 2 == 0) {
+                binNum.unshift('0');
+                decNum = decNum / 2;
+            }
+            else if(decNum % 2 == 1) {
+                binNum.unshift('1');
+                decNum = (decNum - 1) / 2;
+            }
+        }
+
+        arr[i] = binNum.join('');
+    }
     return arr;
 }
 
